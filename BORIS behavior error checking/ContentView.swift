8719095570f52032669a68f1fee0fcc98e8fbf79
Joseph Maa <josephgmaa@berkeley.edu>
2020-10-14 11:48:43 -0700

    Initial Commit

---
+++ BORIS behavior error checking/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BORIS behavior error checking
+//
+//  Created by Joseph Maa on 10/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
